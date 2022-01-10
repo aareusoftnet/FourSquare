53487cf3ffabf9cb88f18095fe9af3a903c37b70
AareuSoftnet <90060278+aareusoftnet@users.noreply.github.com>
2022-01-10 22:09:30 +0530

    - FINISH UIs for the AboutUs screen. (#3)

--- SourceCode/FourSquare/FourSquare/ViewControllers/AboutUs/AboutUsVC.swift
+++ SourceCode/FourSquare/FourSquare/ViewControllers/AboutUs/AboutUsVC.swift
@@ -14,3 +14,21 @@ class AboutUsVC: ParentVC {
         super.viewDidLoad()
     }
 }
+
+//MARK: IBAction(s)
+extension AboutUsVC {
+    
+    @IBAction func onContactNumberTap(_ sender: UIButton) {
+        if let url = URL(string: "tel://+919016813081"),
+           UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    @IBAction func onEmailTap(_ sender: UIButton) {
+        if let url = URL(string: "mailto://vipul.patel251@gmail.com"),
+           UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
